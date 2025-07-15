fdca64d1b1537c2e24bcb9288f5bd54146a35cb1
unknown <xeldhubz@gmail.com>
2025-07-15 02:16:34 +0100

    Grid background

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.glsl"],
   base: "/"
     // Uncomment the following line if you want to use a custom base path
 });
